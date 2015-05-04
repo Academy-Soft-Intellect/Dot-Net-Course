db21212b011891e3dc77bce0f680e5881ae3d42c
Babalev <ivaylo.babalev@gmail.com>
2015-05-04 21:52:15 +0300

    Examples

---
+++ Examples/Welcome-To-Csharp/IntroToNamespaces/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToNamespaces
+{
+    public class TestProgram
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void TestMethod()
+        {
+        }
+    }
+}
+
---
+++ Examples/Welcome-To-Csharp/IntroductionToCsharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductionToCsharp
+{
+    class Program
+    {
+        private static string testString = "Mode";
+
+        static void Main()
+        {
+            Console.WriteLine("This");
+            Console.WriteLine();
+            Console.WriteLine("Is");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine(testString);
+
+
+            int test = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(test);
+        }
+    }
+}
