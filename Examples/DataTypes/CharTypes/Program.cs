bd09eb5ca673336ff319093367b8dc536d3a278a
Babalev <ivaylo.babalev@gmail.com>
2015-05-08 21:50:35 +0300

    String.contains

--- Examples/DataTypes/CharTypes/Program.cs
+++ Examples/DataTypes/CharTypes/Program.cs
@@ -10,15 +10,22 @@ namespace CharTypes
     {
         static void Main(string[] args)
         {
-            //char
-            char c = 'Г';
-            Console.WriteLine(c);
+            ////char
+            //char c = 'Г';
+            //Console.WriteLine(c);
 
-            //string
-            string str = "12345";
-            string str2 = " 6789";
-            string str3 = str + str2;
-            Console.WriteLine(str + str2);
+            ////string
+            //string str = "12345";
+            //string str2 = " 6789";
+            //string str3 = str + str2;
+            //Console.WriteLine(str + str2);
+
+
+
+            string a = "Hello";
+            string b = "elb";
+            Console.WriteLine(a.Contains(b));
+
         }
     }
 }
