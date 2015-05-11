2af743961608af409e9a89c11141d2efad6c95fa
Babalev <ivaylo.babalev@gmail.com>
2015-05-11 21:57:28 +0300

    Loops examples

---
+++ Examples/Loops/DoWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            do
+            {
+                Console.WriteLine(a);
+                a--;
+            } while (a>0);
+        }
+    }
+}
---
+++ Examples/Loops/OneToHundred/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneToHundred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i%7==0 || i%11==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
