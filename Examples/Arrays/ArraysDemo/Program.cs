8fb45554eb0a2e4b06bc3ec696ff010bddba0bdf
Babalev <ivaylo.babalev@gmail.com>
2015-05-15 19:59:42 +0300

    Arrays Examples

---
+++ Examples/Arrays/ArraysDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraysDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] ourArray = new int[6];
+
+            //for (int index = 0; index < ourArray.Length; index++)
+            //{
+            //    Console.WriteLine(ourArray[index]);
+            //}
+
+            //int[] ourSecondArray = { 1, 2, 3, 4 };
+            //Console.WriteLine(ourSecondArray.Length);
+
+            int[] arr = new int[5];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(arr[3]);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+
+
+        }
+    }
+}
---
+++ Examples/Arrays/ForEach/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ourSecondArray = { 1, 2, 3, 4 };
+
+            foreach (int number in ourSecondArray)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
