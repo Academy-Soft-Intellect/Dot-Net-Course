81b6c0d70a19d68d09bb3e989d4d9619a0410a4a
Babalev <ivaylo.babalev@gmail.com>
2015-05-18 17:12:03 +0300

    Lecture 6

---
+++ Examples/ClassesAndObjects/Encapsulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point myPoint = new Point(2, 3);
+            double myPointXCoordinate = myPoint.X; 
+            double myPointYCoordinate = myPoint.Y; 
+            Console.WriteLine("The X coordinate is: " +
+            myPointXCoordinate);
+            Console.WriteLine("The Y coordinate is: " +
+            myPointYCoordinate);
+        }
+    }
+}
---
+++ Examples/ClassesAndObjects/MyFirstClass/Dog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstClass
+{
+    class Dog
+    {
+        public static string Poroda = "Huskey";
+
+        private string name;
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        
+        public Dog()
+        {
+            this.name = "Unnamed";
+        }
+        public Dog(string name)
+        {
+            this.name = name;
+        }
+
+        public void SayBau()
+        {
+            Console.WriteLine("Dog {0} said: BAUUUUU",name);
+        }
+
+    }
+}
