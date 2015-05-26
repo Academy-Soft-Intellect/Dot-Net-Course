62a9a3c528d17d3e6a1029582d21b8199701b8e0
Babalev <ivaylo.babalev@gmail.com>
2015-05-26 15:04:07 +0300

    Methods and recursion

---
+++ Examples/Recursion/Fibonacci/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static long Fib(int n)
+        {
+            if (n<=2)
+            {
+                return 1;
+            }
+            return Fib(n - 1) + Fib(n - 2);
+        }
+    }
+}
