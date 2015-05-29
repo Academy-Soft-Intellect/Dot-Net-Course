9a7048c02d1f3f0661143e33b1228e1de440fb86
Babalev <ivaylo.babalev@gmail.com>
2015-05-29 18:57:31 +0300

    Strings and text

---
+++ Examples/String/String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "aaa";
+            string str2 = "aaa";
+            Console.WriteLine(str1 == str2);
+
+            StreamReader reader = new StreamReader("asdadsa");
+
+
+            reader.ReadToEnd()
+        }
+    }
+}
