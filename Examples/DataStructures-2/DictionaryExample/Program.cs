7194128eacce1c4a5e0e6fff55499f51e6995dc3
Babalev <ivaylo.babalev@gmail.com>
2015-06-05 12:11:11 +0300

    Design Patterns

---
+++ Examples/DataStructures-2/DictionaryExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> dict = new Dictionary<int, string>();
+
+
+            dict.Add(3, "Three");
+            dict.Add(1, "One");
+            dict.Add(4, "Four");
+            dict.Add(2, "Two");
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Value);
+            }
+        }
+    }
+}
---
+++ Examples/DataStructures-2/HashSetExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSetExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = { "cat", "dog", "cat", "leopard", "tiger", "cat" };
+
+            Console.WriteLine(string.Join(",", array1));
+
+            var hash = new HashSet<string>(array1);
+
+            string[] array2 = hash.ToArray();
+
+            Console.WriteLine(string.Join(",", array2));
+
+            hash.Add("dog");
+
+            Console.WriteLine(string.Join(",", array2));
+
+        }
+    }
+}
