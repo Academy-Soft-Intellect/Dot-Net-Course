99d6ade3d2a360b7f646296494ff39a616af0840
Babalev <ivaylo.babalev@gmail.com>
2015-07-21 19:32:20 +0300

    Conditions and Loops

---
+++ Examples/Conditions/01/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditions01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5;
+            string str = "Hello Mark";
+            if (str == "Hello")
+            {
+                i++;
+            }
+            else if (str=="Hello Joe")
+            {
+                i += 2;
+            }
+
+            Console.WriteLine(i);
+        }
+    }
+}
