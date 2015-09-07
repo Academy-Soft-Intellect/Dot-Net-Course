cb437c6f87401dcd445d23c30bc6e91e021cc455
Babalev <ivaylo.babalev@gmail.com>
2015-09-07 08:14:12 +0300

    Exceptions

---
+++ Examples/ExceptionsHandling/ResourceFree/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResourceFree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+
+            if (a==3 || ++b==8)
+            {
+                a += b++;
+            }
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
---
+++ Examples/ExceptionsHandling/UserAndEmail/PasswordException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserAndEmail
+{
+    class PasswordException: Exception
+    {
+        public PasswordException(String message)
+            : base(message)
+        {
+        }
+    }
+}
