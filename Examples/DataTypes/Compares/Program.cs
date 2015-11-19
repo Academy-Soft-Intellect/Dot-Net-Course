26bcc0c0e5fe87ade8b10e750057f01c9ad53a29
Babalev <ivaylo.babalev@gmail.com>
2015-11-19 18:55:19 +0200

    DataTypes

---
+++ Examples/DataTypes/Compares/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    int a = 5;
+        //    int b = 6;
+
+        //    Console.WriteLine(a!=b);
+
+            string z = "Computer";
+            string x = "computer";
+
+            Console.WriteLine(z.Length <= x.Length);
+
+        }
+    }
+}
