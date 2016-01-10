9dd66903396ae5f4c3bd419ce1780fbe547f7e1e
Babalev <ivaylo.babalev@gmail.com>
2016-01-10 22:02:35 +0200

    Data structures

---
+++ Examples/DataStructures/HashTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> test = new Dictionary<string, int>();
+
+            test.Add("cat", 2);
+            test.Add("dog", 1);
+            test.Add("llama", 0);
+            test.Add("iguana", -1);
+
+            if (test.ContainsKey("cat"))
+            {
+                test["cat"]++;
+            }
+        }
+    }
+}
---
+++ Examples/DataStructures/ListExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < 11; i++)
+            {
+                numbers.Add(i);
+            }
+            Console.WriteLine(numbers.Capacity);
+            Console.WriteLine(numbers.Count);
+            numbers.Add(45);
+
+            numbers.Add(12);
+            numbers.Add(12);
+            numbers.Add(12);
+            numbers.Add(12);
+            numbers.Add(12);
+
+            Console.WriteLine(numbers.Capacity);
+            Console.WriteLine(numbers.Count);
+
+
+
+        }
+    }
+}
