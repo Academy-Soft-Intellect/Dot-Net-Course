8b71ef42b06e1ce9c817fca8902da02ec83aa314
Babalev <ivaylo.babalev@gmail.com>
2016-01-18 06:15:39 +0200

    Exceptions

---
+++ Examples/ExceptionsHandling/ExceptionDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            //ReadFile("ttt.txt");
+        }
+        static void ReadFile(string filename)
+        {
+            StreamReader reader = new StreamReader(filename);
+            string line = reader.ReadLine();
+            Console.WriteLine(line);
+            reader.Close();
+        }
+    }
+}
