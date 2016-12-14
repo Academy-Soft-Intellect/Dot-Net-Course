87f4e91900b41ff7ff9cc7736354cf5341e9d149
babalev <ivaylo.babalev@gmail.com>
2016-12-14 19:32:31 +0200

    Arrays and loops

---
+++ Examples/Loops/For/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter a number:");
+            //int n = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter second number:");
+            //int k = int.Parse(Console.ReadLine());
+
+            //for (int index = 10; index >= 0; index--)
+            //{
+            //    Console.WriteLine(index);
+            //}
+
+            //int counter = 0;
+
+            //for (int i = 0, l = 0; i <= 15 && l < 12; i++, l++)
+            //{
+            //    for (int j = 0; j < k; j++)
+            //    {
+            //        for (int m = 0; m < 100; m++)
+            //        {
+            //            Console.WriteLine(++counter);
+            //        }
+            //    }
+            //}
+
+
+            for (int i = 0, l = 0; i <= 15 && l < 12; i++, l++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
