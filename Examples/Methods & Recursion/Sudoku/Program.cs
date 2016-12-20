8c817b3679fa82f993396f7a8b201f7021fe0ba8
babalev <ivaylo.babalev@gmail.com>
2016-12-20 18:25:41 +0200

    Methods

---
+++ Examples/Methods & Recursion/Sudoku/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    class Program
+    {
+        private static SudokuGrid grid = new SudokuGrid();
+        static void Main(string[] args)
+        {
+            PopulateGrid();
+            Console.WriteLine(grid.ToString());
+            Console.WriteLine("Hit Enter to Begin Solving!!");
+            Console.Read();
+            // Start Solving.
+            grid.Solve();
+            Console.WriteLine("Done !!!");
+            Console.ReadLine();
+        }
+    }
+}
